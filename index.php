fd48b85e2762bd03ef3ecccde740a6b5bf175b56
zGiusee <giusebelforte@gmail.com>
2024-01-26 16:39:11 +0100

    [UPDATE] Milestone 1 done (involontariamente con i bonus che non esistono)

--- index.php
+++ index.php
@@ -1,26 +1,59 @@
 <?php
 
-if (isset($_GET['pass_lenght'])) {
+if (isset($_GET['pass_lenght']) && ($_GET['pass_lenght'] >= 4)) {
 
+    // Recupero il valore inserito dall'utente
     $pass_lenght = $_GET['pass_lenght'];
 
     function generatePassword($pass_lenght)
     {
+        // Funzione che mi genera almeno 1 carattere per tipo
+        function random4()
+        {
+            // Stringhe da cui prendere i caratteri
+            $alphabet = 'abcdefghijklmnopqrstuvwxyz';
+            $alphabetCaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $nums = '1234567890 ';
+            $symbols = '?!.,/[]{}()&@#';
 
+            // Funzione che mi recupera 1 carattere dalla stringa in combinazione con funzione randomica
+            $required1 = substr($alphabet,  rand(0, strlen($alphabet) - 1), 1);
+            $required2 = substr($alphabetCaps,  rand(0, strlen($alphabetCaps) - 1), 1);
+            $required3 = substr($nums,  rand(0, strlen($nums) - 1), 1);
+            $required4 = substr($symbols,  rand(0, strlen($symbols) - 1), 1);
 
-        $char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!.,£%&@#';
-        $charLeng = strlen($char) - 1;
+            return $required1 . $required2 . $required3 . $required4;
+        }
 
+        // Richiamo la funzione dei 4 caratteri obbligatori e le assegno ad una variabile
+        $required4 = random4();
+
+        // Insieme di tutte le stringhe di caratteri
+        $allChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!.,/[]{}()&@#';
+
+        // Recupero la lunghezza della stringa che contiene tutti i caratteri
+        $allCharLeng = strlen($allChar) - 1;
+
+        // Creo un array che conterra i caratteri non obbligatori
         $password = [];
 
 
-        for ($i = 0; $i < $pass_lenght; $i++) {
-            $password[] = $char[random_int(0, $charLeng)];
+        for ($i = 0; $i < ($pass_lenght - 4); $i++) {
+            $password[] = $allChar[random_int(0, $allCharLeng)];
         };
 
-        return implode($password);
+        // Trasformo l'array in una stringa
+        $password = implode($password);
+
+        // Unisco i caratteri obbligatori con i caratteri non obbligatori in una stringa unica
+        $final_pass = $password . $required4;
+
+        // Eseguo uno shuffle finale della parola
+        $final_pass = str_shuffle($final_pass);
+
+        return $final_pass;
     }
-}
+};
 
 ?>
 
@@ -46,7 +79,8 @@ if (isset($_GET['pass_lenght'])) {
             </form>
         </div>
         <div>
-            <h5> <?php echo generatePassword($pass_lenght) ?></h5>
+
+            <h5 class="p-4">La tua password è: <?php echo ($_GET['pass_lenght'] <= 3) ? 'La password che hai inserito è troppo corta!' : generatePassword($pass_lenght) ?></h5>
         </div>
     </main>
 </body>
